e345dbd1592158b07cc563f2c56070edbfa08467
Steven Lu <stevenlu443@gmail.com>
2013-08-21 16:38:50 -0400

    Adding this back in because i figure the aliases should go in .vim because it is a cross-shell thing

---
+++ aliases.sh
@@ -0,0 +1,20 @@
+alias l="ls"
+alias gs="git s" # short status 
+alias glp="git log -p --no-ext-diff"
+alias gdt="git difftool"
+alias gd="git diff --no-ext-diff"
+alias gdc="gd --cached"
+alias gg="git lg"
+alias gcm="git commit -am"
+unalias gcp # I rarely cherry pick
+# I often want to commit and push in one step
+gcp() { [ -n "$2" ] && echo "args" && return; git commit -am $1 && git push }
+alias mk="make"
+
+#Dupes of useful ones from omz
+alias gl="git pull"
+alias gp="git push"
+alias gst="git status"
+
+alias ds="dirs -v | head -10"
+alias d="gd"
