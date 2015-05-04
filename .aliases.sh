244e1ede17d10cdacb60e7f547610981a64d54e3
Steven Lu (PuTTY Win7 on Centos 7 VM Feb26[tmux]) <stevenlu443@gmail.com>
2015-05-03 22:20:25 -0400

    ok here we go. finally

--- .aliases.sh
+++ .aliases.sh
@@ -67,7 +67,7 @@ fi
 export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
 export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
 export LESS_TERMCAP_me=$'\E[0m'           # end mode
-export LESS_TERMCAP_se=$'\E[49m'           # end standout-mode
-export LESS_TERMCAP_so=$'\E[48;5;220m'    # begin standout-mode - info box
+export LESS_TERMCAP_se=$'\E[49m'          # end standout-mode (clear bg)
+export LESS_TERMCAP_so=$'\E[48;5;124m'     # begin standout-mode / info box (use bgcolor)
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
 export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
