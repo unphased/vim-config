ff02efdc776405465cd9b62079d36ac1d7af61b8
Steven Lu (1950X) <stevenlu443+1950x@gmail.com>
2020-11-04 11:07:52 -0500

    - old bugs fixed - new bugs added

--- .aliases.sh
+++ .aliases.sh
@@ -18,14 +18,16 @@ alias ls 2>/dev/null >/dev/null || alias ls="ls --color=always"
 # holy grail for my usual envs. Still not gonna have direct vim clipboard 
 # compatibility (that's vim-specific, and can interact with this), but it def 
 # makes life easier in the shell.
-if [ "$(uname)" = Linux ] && ! [[ "$PATH" = *"/linux_pb"* ]] && ! xset q &>/dev/null; then
-	# This ensures that these fallback filesystem clipboard programs become 
-	# available for vim to use if X is not running, regardless of whether 
-	# X exists on the system. In particular we insert linux_pb earlier in the 
-	# path than /usr/local/bin should be, and the environment will control 
-	# which program is pulled on a system.
-	PATH="$HOME/util/linux_pb:$PATH"
-fi
+# removing this because starting a shell is not a good place to limit xclip from working -- the 
+# impl of actual pbcopy and pbpaste can actually attempt both methods
+# if [ "$(uname)" = Linux ] && ! [[ "$PATH" = *"/linux_pb"* ]] && ! xset q &>/dev/null; then
+# 	# This ensures that these fallback filesystem clipboard programs become 
+# 	# available for vim to use if X is not running, regardless of whether 
+# 	# X exists on the system. In particular we insert linux_pb earlier in the 
+# 	# path than /usr/local/bin should be, and the environment will control 
+# 	# which program is pulled on a system.
+# 	PATH="$HOME/util/linux_pb:$PATH"
+# fi
 
 if [ "$(uname)" = Linux ] && lsb_release -i | grep Ubuntu; then
 	alias open="xdg-open"
