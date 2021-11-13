5d1dfb0ebd01d6b947dfd31f30bf62227bb6031b
Steven Lu (MBP M1 Max) <stevenlu443@gmail.com>
2021-11-13 01:41:23 -0500

    - old bugs fixed - new bugs added

--- play-with-git-diff-moved/cpp.cpp
+++ play-with-git-diff-moved/cpp.cpp
@@ -1,64 +1,74 @@
+#include  <string>
+
 int i[] = {
-  1
-  2
-  3
-  4
-  5
-  6
-  7
-  8
-  9
-  10
-  11
-  12
-  13
-  14
-  15
-  16
-  17
-  18
-  19
-  20
-  21
-  22
-  23
-  24
-  25
-  26
-  27
-  28
-  29
-  30
-  31
-  32
-  33
-  34
-  35
-  36
-  37
-  38
-  39
-  40
-  41
-  42
-  43
-  44
-  45
-  46
-  47
-  48
-  49
-  50
-  51
-  52
-  53
-  54
-  55
-  56
-  57
-  58
-  59
-  60
-  61
-  62
+  1,  // comment
+  2,  // comment
+  3,  // comment
+  4,  // comment
+  5,  // comment
+  6,  // comment
+  7,  // comment
+  8,  // comment
+  9,  // comment
+  10,  // comment
+  11,  // comment
+  12,  // comment
+  13,  // comment
+  14,  // comment
+  15,  // comment
+  16,  // comment
+  17,  // comment
+  18,  // comment
+  19,  // comment
+  20,  // comment
+  21,  // comment
+  22,  // comment
+  23,  // comment
+  24,  // comment
+  25,  // comment
+  26,  // comment
+  27,  // comment
+  28,  // comment
+  29,  // comment
+  30,  // comment
+  31,  // comment
+  32,  // comment
+  33,  // comment
+  34,  // comment
+  35,  // comment
+  36,  // comment
+  37,  // comment
+  38,  // comment
+  39,  // comment
+  40,  // comment
+  41,  // comment
+  42,  // comment
+  43,  // comment
+  44,  // comment
+  45,  // comment
+  46,  // comment
+  47,  // comment
+  48,  // comment
+  49,  // comment
+  50,  // comment
+  51,  // comment
+  52,  // comment
+  53,  // comment
+  54,  // comment
+  55,  // comment
+  56,  // comment
+  57,  // comment
+  58  // comment
+};
+
+std::string[] ss = {
+  "abc", // abc
+  "def", // def
+  "def1", // def
+  "def2", // def
+  "def3", // def
+  "def4", // def
+  "def5", // def
+  "def6", // def
+  "def7", // def
 };
