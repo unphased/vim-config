1dbd139161208d5de93bfd8feb7a22f5c67adde7
Steven Lu (MBP M1 Max) <stevenlu443@gmail.com>
2023-02-04 04:35:10 -0500

    so good

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,18 @@
+return {
+  {
+     "nvim-neo-tree/neo-tree.nvim",
+     branch = "v2.x",
+     dependencies = { 
+       "nvim-lua/plenary.nvim",
+       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+       "MunifTanjim/nui.nvim",
+     }
+  },
+  "tpope/vim-surround",
+  "tpope/vim-sleuth",
+  "tpope/vim-repeat",
+  "tpope/vim-commentary",
+  'AndrewRadev/switch.vim',
+  'junegunn/fzf.vim',
+  'github/copilot.vim'
+}
