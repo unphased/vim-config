2c1fbd07ec12a15f6ebd25284d604a83748c3715
Steven Lu (MBP M1 Max) <stevenlu443@gmail.com>
2023-02-04 04:14:07 -0500

    progress

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.cmd([[ colorscheme habamax ]])
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,3 +13,44 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+
+-- plugins
+
+require("lazy").setup({
+  {
+     "nvim-neo-tree/neo-tree.nvim",
+     branch = "v2.x",
+     dependencies = { 
+       "nvim-lua/plenary.nvim",
+       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+       "MunifTanjim/nui.nvim",
+     }
+  },
+  "tpope/vim-surround",
+  "tpope/vim-sleuth",
+  'AndrewRadev/switch.vim',
+  'junegunn/fzf.vim'
+}, {
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = true, -- get a notification when changes are found
+  },
+})
+
+-- mappings
+
+vim.keymap.set('n', 'k', 'gk')
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'gk', 'k')
+vim.keymap.set('n', 'gj', 'j')
+
+vim.keymap.set('n', '<c-n>', function()
+
+end
+)
+
+-- settings
+
+vim.o.number = true
+
