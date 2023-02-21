de27d9895dd4bfe2986042e3e04974524bfd2125
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-02-20 21:20:17 -0500

    something

--- nvim/init.lua
+++ nvim/init.lua
@@ -410,7 +410,9 @@ vim.opt.list = true
 vim.opt.listchars = "tab:→ ,extends:»,precedes:«,trail:·,nbsp:◆"
 
 require("indent_blankline").setup {
-  space_char_blankline = " ",
+  char='▏',
+  context_char='▏',
+  space_char_blankline=' ',
   show_current_context = true,
   show_current_context_start = true,
 }
