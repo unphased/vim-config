63bcfab3ebde3c3ac782f8ac8ed62510cac69ffa
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-02-26 21:46:00 -0500

    and the corresponding init side

--- nvim/init.lua
+++ nvim/init.lua
@@ -493,143 +493,37 @@ require("indent_blankline").setup {
 
 -- nvim-lsp via cmp
 
-local cmp = require'cmp'
-local lspkind = require('lspkind')
-
-cmp.setup({
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '…', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      -- before = function (entry, vim_item)
-      --   ...
-      --   return vim_item
-      -- end
-    })
-  },
-
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
-    }),
-    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'copilot' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-    { name = 'buffer' },
-  })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
--- Set up lspconfig.
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['clangd'].setup {
---   capabilities = capabilities
+-- require("mason-lspconfig").setup_handlers {
+--   -- The first entry (without a key) will be the default handler
+--   -- and will be called for each installed server that doesn't have
+--   -- a dedicated handler.
+--   function (server_name) -- default handler (optional)
+--     require("lspconfig")[server_name].setup {}
+--   end,
+--   -- Next, you can provide a dedicated handler for specific servers.
+--   ["lua_ls"] = function ()
+--     require("lspconfig")["lua_ls"].setup {
+--       settings = {
+--         Lua = {
+--           diagnostics = {
+--             enable = true,
+--             globals = { 'vim' }
+--           },
+--           runtime = {
+--             version = 'LuaJIT',
+--           },
+--           workspace = {
+--             library = vim.api.nvim_get_runtime_file('', true),
+--             maxPreload = 10000,
+--             preloadFileSize = 10000,
+--             checkThirdParty = false,
+--           },
+--           telemetry = {enable = false},
+--         }
+--       }
+--     }
+--   end
 -- }
-
-require("mason").setup({})
-require("mason-lspconfig").setup({
-  -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
-  -- This setting has no relation with the `automatic_installation` setting.
-  ensure_installed = {
-    "clangd", "lua_ls"
-  },
-
-  -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
-  -- This setting has no relation with the `ensure_installed` setting.
-  -- Can either be:
-  --   - false: Servers are not automatically installed.
-  --   - true: All servers set up via lspconfig are automatically installed.
-  --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
-  --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
-  automatic_installation = true,
-})
-require("mason-lspconfig").setup_handlers {
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
-  function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {}
-  end,
-  -- Next, you can provide a dedicated handler for specific servers.
-  ["lua_ls"] = function ()
-    require("lspconfig")["lua_ls"].setup {
-      settings = {
-        Lua = {
-          diagnostics = {
-            enable = true,
-            globals = { 'vim' }
-          },
-          runtime = {
-            version = 'LuaJIT',
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file('', true),
-            maxPreload = 10000,
-            preloadFileSize = 10000,
-            checkThirdParty = false,
-          },
-          telemetry = {enable = false},
-        }
-      }
-    }
-  end
-}
 
 -- local null_ls = require('null-ls')
 --
@@ -641,51 +535,51 @@ require("mason-lspconfig").setup_handlers {
 --     null_ls.builtins.diagnostics.selene,
 --   }
 -- })
-local null_ls = require 'null-ls'
+-- local null_ls = require 'null-ls'
 
-require("mason-null-ls").setup({
-    ensure_installed = { "shellcheck" },
-    automatic_setup = true
-})
-require 'mason-null-ls'.setup_handlers {
-    function(source_name, methods)
-      -- print("mason-null-ls-handler: source_name:" .. source_name)
-      -- print("mason-null-ls-handler: methods:" .. vim.inspect(methods))
-      -- all sources with no handler get passed here
+-- require("mason-null-ls").setup({
+--     ensure_installed = { "shellcheck" },
+--     automatic_setup = true
+-- })
+-- require 'mason-null-ls'.setup_handlers {
+--     function(source_name, methods)
+--       -- print("mason-null-ls-handler: source_name:" .. source_name)
+--       -- print("mason-null-ls-handler: methods:" .. vim.inspect(methods))
+--       -- all sources with no handler get passed here
+--
+--       -- To keep the original functionality of `automatic_setup = true`,
+--       -- please add the below.
+--       require("mason-null-ls.automatic_setup")(source_name, methods)
+--     end,
+--
+--     -- stylua = function(source_name, methods)
+--     --   null_ls.register(null_ls.builtins.formatting.stylua)
+--     -- end,
+--
+--     ---- Semgrep is cool but takes way long to run (need to find out how to extend timeout) and also does not commonly emit much output. So I'm not really interested in it right now.
+--     -- semgrep = function(source_name, methods)
+--     --   null_ls.register(null_ls.builtins.diagnostics.semgrep.with({
+--     --     extra_args = { "--config", "auto" },
+--     --   }))
+--     -- end,
+--     --
+--     -- print("semgrep obtained as:", vim.inspect(null_ls.builtins.diagnostics.semgrep)),
+--     -- print("semgrep prospective:", vim.inspect(null_ls.builtins.diagnostics.semgrep.with({
+--     --   extra_args = { "--config", "auto" },
+--     -- })))
+-- }
 
-      -- To keep the original functionality of `automatic_setup = true`,
-      -- please add the below.
-      require("mason-null-ls.automatic_setup")(source_name, methods)
-    end,
-
-    -- stylua = function(source_name, methods)
-    --   null_ls.register(null_ls.builtins.formatting.stylua)
-    -- end,
-
-    ---- Semgrep is cool but takes way long to run (need to find out how to extend timeout) and also does not commonly emit much output. So I'm not really interested in it right now.
-    -- semgrep = function(source_name, methods)
-    --   null_ls.register(null_ls.builtins.diagnostics.semgrep.with({
-    --     extra_args = { "--config", "auto" },
-    --   }))
-    -- end,
-    --
-    -- print("semgrep obtained as:", vim.inspect(null_ls.builtins.diagnostics.semgrep)),
-    -- print("semgrep prospective:", vim.inspect(null_ls.builtins.diagnostics.semgrep.with({
-    --   extra_args = { "--config", "auto" },
-    -- })))
-}
-
-null_ls.setup({
-  debug = true,
-})
-
-require("copilot").setup({
-  -- suggestion = { enabled = false },
-  -- panel = { enabled = false },
-})
-require("copilot_cmp").setup({
-  method = "getCompletionsCycling",
-})
+-- null_ls.setup({
+--   debug = true,
+-- })
+--
+-- require("copilot").setup({
+--   -- suggestion = { enabled = false },
+--   -- panel = { enabled = false },
+-- })
+-- require("copilot_cmp").setup({
+--   method = "getCompletionsCycling",
+-- })
 
 -- helper
 function string:split(delimiter)
