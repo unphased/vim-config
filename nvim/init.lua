ccb697577d288737df5637148e35a1e0ea76411d
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-03-04 23:54:51 -0500

    lets drive this for a little bit

--- nvim/init.lua
+++ nvim/init.lua
@@ -648,12 +648,6 @@ require("indent_blankline").setup({
 -- -- Snippet config
 -- ---
 --
--- require("luasnip").config.set_config({
---   region_check_events = "InsertEnter",
---   delete_check_events = "InsertLeave",
--- })
---
--- require("luasnip.loaders.from_vscode").lazy_load()
 --
 -- ---
 -- -- Autocompletion
@@ -714,6 +708,19 @@ vim.opt.completeopt="menu,menuone,noselect"
 local cmp = require'cmp'
 local lspkind = require('lspkind')
 
+local luasnip = require("luasnip")
+-- luasnip.config.set_config({
+  -- region_check_events = "InsertEnter",
+  -- delete_check_events = "InsertLeave",
+-- })
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
   formatting = {
     format = lspkind.cmp_format({
@@ -728,6 +735,11 @@ cmp.setup({
       --   return vim_item
       -- end
     })
+  },
+  snippet = {
+    expand = function(args)
+      require'luasnip'.lsp_expand(args.body)
+    end
   },
 
   window = {
@@ -744,13 +756,34 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
     }),
-    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+      -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
+    { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
     { name = 'buffer' },
@@ -799,7 +832,6 @@ cmp.setup.cmdline(':', {
 
 -- insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
 cmp.event:on(
   'confirm_done',
   cmp_autopairs.on_confirm_done()
