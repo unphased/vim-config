8a64a45f260a482b65c786b5e229651d668a097a
Steven Lu (5950X 20.04) <stevenlu443@gmail.com>
2023-03-12 23:35:08 -0400

    hmmmm

--- nvim/lua/heirline_conf/heirline.lua
+++ nvim/lua/heirline_conf/heirline.lua
@@ -535,44 +535,45 @@ local Git = {
 
     hl = { fg = "orange" },
 
-
-    {   -- git branch name
-        provider = function(self)
-            return " " .. self.status_dict.head
-        end,
-        hl = { bold = true }
-    },
-    -- You could handle delimiters, icons and counts similar to Diagnostics
+    utils.surround({ "", "" }, "muted_bg", 
     {
-        condition = function(self)
-            return self.has_changes
-        end,
-        provider = " "
-    },
-    {
-        provider = function(self)
-            local count = self.status_dict.added or 0
-            return count > 0 and ("+" .. count)
-        end,
-        hl = { fg = "git_add" },
-    },
-    {
-        provider = function(self)
-            local count = self.status_dict.removed or 0
-            return count > 0 and ("-" .. count)
-        end,
-        hl = { fg = "git_del" },
-    },
-    {
-        provider = function(self)
-            local count = self.status_dict.changed or 0
-            return count > 0 and ("~" .. count)
-        end,
-        hl = { fg = "git_change" },
-    },
+        {   -- git branch name
+            provider = function(self)
+                return " " .. self.status_dict.head
+            end,
+            hl = { bold = true }
+        },
+        -- You could handle delimiters, icons and counts similar to Diagnostics
+        {
+            condition = function(self)
+                return self.has_changes
+            end,
+            provider = " "
+        },
+        {
+            provider = function(self)
+                local count = self.status_dict.added or 0
+                return count > 0 and ("+" .. count)
+            end,
+            hl = { fg = "git_add" },
+        },
+        {
+            provider = function(self)
+                local count = self.status_dict.removed or 0
+                return count > 0 and ("-" .. count)
+            end,
+            hl = { fg = "git_del" },
+        },
+        {
+            provider = function(self)
+                local count = self.status_dict.changed or 0
+                return count > 0 and ("~" .. count)
+            end,
+            hl = { fg = "git_change" },
+        },
+    })
 }
 
-Git = utils.surround({ "", "" }, "muted_bg", Git)
 
 -- local DAPMessages = {
 --     condition = function()
