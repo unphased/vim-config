9a1ab219adab062c17b5096cfb2d0dad27e2573a
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-03-14 03:33:29 -0400

    bring more things in that i missed

--- nvim/init.lua
+++ nvim/init.lua
@@ -271,6 +271,48 @@ vim.cmd([[
 
   "" This enables the count and index indicator for search. Not enabled because we are using google/vim-searchindex and it does not limit to a count of 99
   " set shortmess-=S
+
+  " an intuitive way to surround a selection with newlines
+  vmap S<CR> S<C-J>V2j=
+
+  " The visual search implementation. I haven't seen this in a plugin so I maintain it here slightly modified with my binds
+
+  " Search for selected text.
+  " http://vim.wikia.com/wiki/VimTip171
+  let s:save_cpo = &cpo | set cpo&vim
+  if !exists('g:VeryLiteral')
+    let g:VeryLiteral = 0
+  endif
+  function! s:VSetSearch(cmd)
+    let old_reg = getreg('"')
+    let old_regtype = getregtype('"')
+    normal! gvy
+    if @@ =~? '^[0-9a-z,_]*$' || @@ =~? '^[0-9a-z ,_]*$' && g:VeryLiteral
+      let @/ = @@
+    else
+      let pat = escape(@@, a:cmd.'\')
+      if g:VeryLiteral
+        let pat = substitute(pat, '\n', '\\n', 'g')
+      else
+        let pat = substitute(pat, '^\_s\+', '\\s\\+', '')
+        let pat = substitute(pat, '\_s\+$', '\\s\\*', '')
+        let pat = substitute(pat, '\_s\+', '\\_s\\+', 'g')
+      endif
+      let @/ = '\V'.pat
+    endif
+    normal! gV
+    call setreg('"', old_reg, old_regtype)
+  endfunction
+  vnoremap <silent> <CR> :<C-U>call <SID>VSetSearch('/')<CR>/<C-R>/<CR>
+  vnoremap <silent> # :<C-U>call <SID>VSetSearch('?')<CR>?<C-R>/<CR>
+  vmap <kMultiply> *
+  nmap <silent> <Plug>VLToggle :let g:VeryLiteral = !g:VeryLiteral
+    \\| echo "VeryLiteral " . (g:VeryLiteral ? "On" : "Off")<CR>
+  " if !hasmapto("<Plug>VLToggle")
+  "   nmap <unique> <Leader>vl <Plug>VLToggle
+  " endif
+  let &cpo = s:save_cpo | unlet s:save_cpo
+  " End impl of Search for selected text.
 ]])
 
 -- gvar settings for plugins
