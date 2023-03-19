beff330a9eb584ce99a44f9f0397861ad0e87a80
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-03-19 01:33:48 -0400

    whichkey

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -184,4 +184,16 @@ return {
       -- ]])
     end
   },
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
 }
