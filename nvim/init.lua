57f815cc4f07204a9d87a6e71cc98a5cdb8e888a
Steven Lu (5950X 20.04[tmux]) <stevenlu443@gmail.com>
2023-03-22 18:19:59 -0400

    did it do it? it did it

--- nvim/init.lua
+++ nvim/init.lua
@@ -1056,7 +1056,11 @@ require("mason-lspconfig").setup_handlers {
         Lua = {
           completion = {
             callSnippet = "Replace"
-          }
+          },
+          workspace = {
+            -- library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
+          },
         }
       }
     }
