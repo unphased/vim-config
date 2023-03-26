4a6191adf2e5c7d3859b0c6570a74327ccd69c4a
Steven Lu (5950X 20.04[tmux]) <stevenlu443@gmail.com>
2023-03-26 11:54:17 -0400

    - old bugs fixed - new bugs added

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,7 @@
 - prevent mouse interaction from messing with insert mode cursor position
 - implement insert mode ctrl backspace
 - implement insert mode reliable ctrl/alt arrows
-- implement semantic highlight removal (i want this in possibly lua right now but also definitely dockerfile) by literally selecting them out at the highlight group level
+- implement semantic highlight removal (i want this in possibly lua right now but also definitely dockerfile) by literally selecting them out at the highlight group level (ah dang, no worky for dockerls)
 
 --]]
 
