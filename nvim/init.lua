60c2c1a984aa62b45937e34f782eeaef3073720c
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-04-21 18:10:23 -0400

    - old bugs fixed - new bugs added

--- nvim/init.lua
+++ nvim/init.lua
@@ -507,7 +507,7 @@ require("Comment").setup()
 
 require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "bash", "comment" },
+  ensure_installed = { "c", "lua", "vim", "bash", "comment" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
