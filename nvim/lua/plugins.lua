127983c49a44ec905fdada80afd2506eeee56504
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-07-16 17:41:01 -0400

    - old bugs fixed - new bugs added

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -37,7 +37,7 @@ return {
   -- 'junegunn/fzf',
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, branch = "0.1.x" },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-  'nvim-telescope/telescope-ui-select.nvim', 
+  'nvim-telescope/telescope-ui-select.nvim',
   'norcalli/nvim-colorizer.lua',
   -- {
   --    "zbirenbaum/copilot-cmp",
