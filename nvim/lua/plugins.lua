9f6962b5d90308cc33bf1979184f9f618e79a13c
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2023-08-15 00:47:20 -0400

    yeah

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -51,7 +51,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    commit = "a9ce9fb7cccf59e0c2f65d26a14942356c93948e",
+    tag = "v0.9.1",
     dependencies = {
       -- show treesitter nodes
       "nvim-treesitter/playground", -- enable more advanced treesitter-aware text objects
