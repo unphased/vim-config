329120e200934b8093052396510472673abd4887
Steven Lu (5950X 20.04[tmux]) <stevenlu443@gmail.com>
2023-08-17 18:14:34 -0400

    wanna use the other one made by olrtg actually

--- nvim/init.lua
+++ nvim/init.lua
@@ -1427,7 +1427,22 @@ safeRequire("mason-lspconfig").setup_handlers {
         }
       }
     }
-  end
+  end,
+  -- ["emmet_ls"] = function ()
+  --   safeRequire('lspconfig').emmet_ls.setup({
+  --     on_attach = lsp_attach,
+  --     capabilities = capabilities,
+  --     filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+  --     init_options = {
+  --       html = {
+  --         options = {
+  --           -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+  --           ["bem.enabled"] = true,
+  --         },
+  --       },
+  --     }
+  --   })
+  -- end
 }
 
 safeRequire("yanky").setup({
