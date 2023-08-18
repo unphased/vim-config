8d5a85af5346b64f8514d0ada455be4b8b11d4dc
Steven Lu (5950X 20.04) <stevenlu443@gmail.com>
2023-08-17 21:05:09 -0400

    should be good

--- nvim/init.lua
+++ nvim/init.lua
@@ -1678,13 +1678,33 @@ vim.cmd[[
 ]]
 
 require('snippy').setup({
-    mappings = {
-        is = {
-            ['<Tab>'] = 'expand_or_advance',
-            ['<S-Tab>'] = 'previous',
-        },
-        nx = {
-            ['<leader>x'] = 'cut_text',
-        },
+  mappings = {
+    is = {
+      ['<Tab>'] = 'expand_or_advance',
+      ['<S-Tab>'] = 'previous',
     },
+    nx = {
+      ['<leader>x'] = 'cut_text',
+    },
+  },
+})
+
+require('lspconfig').emmet_language_server.setup({
+  filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+  init_options = {
+    --- @type table<string, any> https://docs.emmet.io/customization/preferences/
+    preferences = {},
+    --- @type "always" | "never" defaults to `"always"`
+    showexpandedabbreviation = "always",
+    --- @type boolean defaults to `true`
+    showabbreviationsuggestions = true,
+    --- @type boolean defaults to `false`
+    showsuggestionsassnippets = false,
+    --- @type table<string, any> https://docs.emmet.io/customization/syntax-profiles/
+    syntaxprofiles = {},
+    --- @type table<string, string> https://docs.emmet.io/customization/snippets/#variables
+    variables = {},
+    --- @type string[]
+    excludelanguages = {},
+  },
 })
