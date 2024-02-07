c8ef5cc314491969b04adccca034bd33f4ef3fcf
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-02-07 14:55:06 +0700

    alrighty

--- nvim/init.lua
+++ nvim/init.lua
@@ -2137,3 +2137,23 @@ end
 
 -- normal shift-X deletes prior to cursor, something i will never use.
 vim.keymap.set('n', 'X', CurBufDel, { desc = "Delete current buffer" })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    local api = require('nvim-tree.api')
+
+    -- Only 1 window with nvim-tree left: we probably closed a file buffer
+    if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+      -- Required to let the close event complete. An error is thrown without this.
+      vim.defer_fn(function()
+        -- close nvim-tree: will go to the last hidden buffer used before closing
+        api.tree.toggle({find_file = true, focus = true})
+        -- re-open nivm-tree
+        api.tree.toggle({find_file = true, focus = true})
+        -- nvim-tree is still the active window. Go to the previous window.
+        vim.cmd("wincmd p")
+      end, 0)
+    end
+  end
+})
