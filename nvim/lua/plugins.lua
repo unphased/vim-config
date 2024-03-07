230e8b67a67f75f6d5ca2a826c7ab70f6d3b3931
Steven Lu (velka 7 22.04) <stevenlu443@gmail.com>
2024-03-07 12:56:30 +0700

    - old bugs fixed - new bugs added

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -385,13 +385,13 @@ return {
     end
   },
   {
-  'bloznelis/before.nvim',
-  config = function()
-    local before = require('before')
-    before.setup()
+    'bloznelis/before.nvim',
+    config = function()
+      local before = require('before')
+      before.setup()
 
-    vim.keymap.set('n', '<C-h>', before.jump_to_last_edit, {})
-    vim.keymap.set('n', '<C-l>', before.jump_to_next_edit, {})
-  end
-}
+      vim.keymap.set('n', '<C-h>', before.jump_to_last_edit, {})
+      vim.keymap.set('n', '<C-l>', before.jump_to_next_edit, {})
+    end
+  }
 }
