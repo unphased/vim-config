7b30f4a20acd9c4ea5f5fdb82e22e1df0f77b407
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-04-16 15:43:25 +0700

    fix

--- nvim/lua/heirline_conf/heirline.lua
+++ nvim/lua/heirline_conf/heirline.lua
@@ -297,7 +297,7 @@ local FileSize = {
                 return fsize..suffix[1]
             end
             local i = math.floor((math.log(fsize) / math.log(1024)))
-            return string.format("%.2g%s", fsize / math.pow(1024, i), suffix[i + 1]) .. ' '
+            return string.format("%.2f%s", fsize / math.pow(1024, i), suffix[i + 1]) .. ' '
         end
     }, {
         provider = ""
