d197f57d36622fb6a6b1324fc95d0e6c2d2961d2
Steven Lu (MBP M1 Max[tmux]) <stevenlu443@gmail.com>
2024-05-15 21:46:29 -0400

    new highlight group via neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1835,6 +1835,7 @@ vim.cmd([[
   hi YankyYanked guibg=#2e5099 gui=bold cterm=bold
 
   hi Search cterm=bold ctermfg=black ctermbg=yellow guibg=#a9291a guifg=NONE
+  hi CurSearch cterm=bold ctermfg=black ctermbg=yellow guibg=#ff392a guifg=NONE
   hi IncSearch cterm=bold ctermfg=black ctermbg=cyan guibg=#f04050 guifg=NONE gui=NONE
   hi Visual term=reverse ctermbg=238 guibg=#555540
   hi NormalFloat guibg=#232336
