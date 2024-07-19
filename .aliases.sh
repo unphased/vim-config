cb81b7dd2da0d78e6d139bbec9bbb637e47b751a
Steven Lu @ MBP-M1-Max <1542910+unphased@users.noreply.github.com>
2024-07-19 00:33:04 -0400

    - old bugs fixed - new bugs added

--- .aliases.sh
+++ .aliases.sh
@@ -213,3 +213,14 @@ aider_function() {
   command aider "$@"
 }
 alias aider='aider_function'
+
+function git-undo() {
+    if [ "$1" = "drop" ]; then
+        git reset --hard HEAD~${2:-1}
+    elif [ "$1" = "revert" ]; then
+        git revert --no-commit HEAD~${2:-1}..HEAD
+        git commit -m "Reverts the last ${2:-1} commit(s)"
+    else
+        echo "Usage: git-undo [drop|revert] [number of commits]"
+    fi
+}
