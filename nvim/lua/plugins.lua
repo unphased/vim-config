fa1899d75048f6d4fe68ead44bc800d7fd2c30c5
Steven Lu @ MBP-M1-Max <1542910+unphased@users.noreply.github.com>
2024-08-15 01:10:27 -0400

    seems better

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -473,5 +473,13 @@ return {
     config = function()
       require("oil").setup()
     end
+  },
+  {
+    "LintaoAmons/bookmarks.nvim",
+    -- tag = "v0.5.4", -- optional, pin the plugin at specific version for stability
+    dependencies = {
+      {"nvim-telescope/telescope.nvim"},
+      {"stevearc/dressing.nvim"} -- optional: to have the same UI shown in the GIF
+    }
   }
 }
