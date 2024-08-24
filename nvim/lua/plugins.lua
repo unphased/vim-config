09963226065bd68e94f887485ce0ebcb08284992
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2024-08-23 22:40:36 -0400

    alright

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -528,5 +528,15 @@ return {
       })
     end
   },
+  {
+    "sourcegraph/sg.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]] },
+
+    -- If you have a recent version of lazy.nvim, you don't need to add this!
+    build = "nvim -l build/init.lua",
+    config = function ()
+      require('sg').setup{}
+    end
+  },
 
 }
