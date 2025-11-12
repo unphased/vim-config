7716809c45f227133664f0ac2600cd61ff04f447
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2025-11-12 01:10:05 -0500

    well this should be it then

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -1,8 +1,25 @@
 ; extends
+
+; WGSL
 (
- (variable_declarator
-  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
-  (template_string (string_fragment) @injection.content)
- )
+  [
+    (variable_declarator (comment) @c value: (template_string (string_fragment)? @injection.content))
+    (assignment_expression (comment) @c right: (template_string (string_fragment)? @injection.content))
+    (arguments (comment) @c (template_string (string_fragment)? @injection.content))
+    (pair (comment) @c value: (template_string (string_fragment)? @injection.content))
+  ]
+  (#match? @c "^(/\\*\\s*wgsl\\s*\\*/|//\\s*wgsl\\s*)$")
+  (#set! injection.language "wgsl")
 )
 
+; GLSL
+(
+  [
+    (variable_declarator (comment) @c value: (template_string (string_fragment)? @injection.content))
+    (assignment_expression (comment) @c right: (template_string (string_fragment)? @injection.content))
+    (arguments (comment) @c (template_string (string_fragment)? @injection.content))
+    (pair (comment) @c value: (template_string (string_fragment)? @injection.content))
+  ]
+  (#match? @c "^(/\\*\\s*glsl\\s*\\*/|//\\s*glsl\\s*)$")
+  (#set! injection.language "glsl")
+)
