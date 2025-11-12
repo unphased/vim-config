19a9e55f5366b658cd4cd212a62ea32e971a8e1a
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2025-11-12 01:08:34 -0500

    seems to work. cleaner.

--- after/queries/javascript/injections.scm
+++ after/queries/javascript/injections.scm
@@ -1,20 +1,13 @@
 ; extends
 (
-  (variable_declarator
-    (comment) @c
-    .
-    value: (template_string (string_fragment)? @injection.content)
-  )
-  (#match? @c "^(/\\*\\s*wgsl\\s*\\*/|//\\s*wgsl\\s*)$")
-  (#set! injection.language "wgsl")
-)
-
-; { key: /* wgsl */ `...` }
-(
-  (pair
-    (comment) @c
-    value: (template_string (string_fragment)? @injection.content)
-  )
+  [
+    (variable_declarator
+      (comment) @c
+      value: (template_string (string_fragment)? @injection.content))
+    (pair
+      (comment) @c
+      value: (template_string (string_fragment)? @injection.content))
+  ]
   (#match? @c "^(/\\*\\s*wgsl\\s*\\*/|//\\s*wgsl\\s*)$")
   (#set! injection.language "wgsl")
 )
