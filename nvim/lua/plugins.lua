139d1cac5351a3cf2c078a921ceb2ec7115bdc77
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2025-12-13 02:05:27 +0700

    nvim: show markdown code fences
    Configure render-markdown.nvim to avoid concealing code block delimiters by setting code.conceal_delimiters=false.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -649,7 +649,11 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+      code = {
+        conceal_delimiters = false,
+      }
+    },
   },
   -- {
   --   "OXY2DEV/markview.nvim",
