e889adc853cb6403810f42da7d89b4b696c7149a
Steven Lu <stevenlu443@gmail.com>
2025-12-19 21:30:50 +0700

    Use dconf file as single source for PaperWM keybindings

--- paperwm-keybindings.apply.sh
+++ paperwm-keybindings.apply.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-gsettings set org.gnome.shell.extensions.paperwm.keybindings switch-left "['<Super>h']"
-gsettings set org.gnome.shell.extensions.paperwm.keybindings switch-down "['<Super>j']"
-gsettings set org.gnome.shell.extensions.paperwm.keybindings switch-up "['<Super>k']"
-gsettings set org.gnome.shell.extensions.paperwm.keybindings switch-right "['<Super>l']"
-gsettings set org.gnome.desktop.wm.keybindings hide-window "[]"
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+dconf load / < "${script_dir}/paperwm-keybindings.dconf"
