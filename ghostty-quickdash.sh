313d27dbc99790f7f8c0881cf2f459fbee1dc121
Steven Lu (MBP M1 Max) <1542910+unphased@users.noreply.github.com>
2026-05-11 10:37:04 -0400

    allow color

--- ghostty-quickdash.sh
+++ ghostty-quickdash.sh
@@ -14,7 +14,7 @@ if ! "$tmux_bin" has-session -t "$session" 2>/dev/null; then
   left_pane=$("$tmux_bin" display-message -p -t "$session" '#{pane_id}')
   "$tmux_bin" send-keys -t "$left_pane" "$htop_bin" C-m
   right_pane=$("$tmux_bin" split-window -h -P -F '#{pane_id}' -t "$left_pane" -c "$home_dir")
-  "$tmux_bin" send-keys -t "$right_pane" "$watch_bin -n 300 $quota_script" C-m
+  "$tmux_bin" send-keys -t "$right_pane" "$watch_bin -c -n 300 $quota_script" C-m
   "$tmux_bin" select-pane -t "$left_pane"
 fi
 
